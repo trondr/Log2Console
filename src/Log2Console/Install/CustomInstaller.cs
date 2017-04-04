7cdd0ee363a21ad43952b63a503cf936824e50d9
trondr <trondr@outlook.com>
2017-04-04 20:56:23 +0200

    Replaced leagacy setup project with Windows Installer Xml setup project from the NCmdLiner.SolutionCreator template. Added custom installer to the Log2Console project to prepare for launching Log2Console from the File Explorer context menu.

---
+++ src/Log2Console/Install/CustomInstaller.cs
@@ -0,0 +1,37 @@
+﻿//Source: https://github.com/trondr/NCmdLiner.SolutionTemplates/tree/8f553ae5d850ced2dfa754d933f42ae6ee1f673b/Console%20Application/src/_S_ConsoleProjectName_S_/Module
+
+using System.Collections;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Reflection;
+
+namespace Log2Console.Install
+{
+    [RunInstaller(true)]
+    public partial class CustomInstaller : Installer
+    {
+        public CustomInstaller()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            //Adding a command to windows explorer contect menu
+            this.Context.LogMessage("Adding Log2Console to File Explorer context menu...");
+            new WindowsExplorerContextMenuInstaller().Install("Log2Console", "Create Something...", Assembly.GetExecutingAssembly().Location, "CreateSomething /exampleParameter=\"%1\"");
+            this.Context.LogMessage("Finnished adding Log2Console to File Explorer context menu.");            
+            base.Install(stateSaver);
+        }
+
+        public override void Uninstall(IDictionary savedState)
+        {
+            //Removing previously installed command from windows explorer contect menu
+            this.Context.LogMessage("Removing Log2Console from File Explorer context menu...");
+            new WindowsExplorerContextMenuInstaller().UnInstall("Log2Console");
+            this.Context.LogMessage("Finished removing Log2Console from File Explorer context menu.");
+            
+            base.Uninstall(savedState);
+        }        
+    }
+}
---
+++ src/Log2Console/Install/IWindowsExplorerContextMenuInstaller.cs
@@ -0,0 +1,11 @@
+﻿//Source: https://github.com/trondr/NCmdLiner.SolutionTemplates/tree/8f553ae5d850ced2dfa754d933f42ae6ee1f673b/Console%20Application/src/_S_LibraryProjectName_S_/Module/Common/Install
+
+namespace Log2Console.Install
+{
+    public interface IWindowsExplorerContextMenuInstaller
+    {
+        void Install(string commandId, string commandName, string command, string arguments);
+
+        void UnInstall(string commandId);
+    }
+}
